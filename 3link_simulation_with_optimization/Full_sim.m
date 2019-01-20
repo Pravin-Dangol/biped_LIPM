d9e9e851b12a8b65d35af353e595f0498bce9ca6
calcutek <43457115+calcutek@users.noreply.github.com>
2019-01-20 05:56:00 -0500

    Add files via upload
    Fixed controller, now it's symbolic rather than partially pre computed

--- 3link_simulation_with_optimization/Full_sim.m
+++ 3link_simulation_with_optimization/Full_sim.m
@@ -1,8 +1,8 @@
 %Simulates full hybrid dynamics with parametrized output function
 function Full_sim()
 %Output of fmincon [q1, q1_dot, alpha,J]
-f = [-0.2618 -1.2 0 0.20 0.5236 2.0944 2.50 2.618];
-M = 4; delq = deg2rad(30);              %difference between min and max q1
+f = [-0.2618 -2 0 0.20 0.5236 2.0944 2.50 2.618];
+M = 4; delq = deg2rad(30); %z_plus = 0;
 
 z_minus = f(1:2);
 %alpha is the Bezier coeffs for q2, gamma for q3
@@ -14,7 +14,7 @@ gamma = f(6:8);         %alpha 3 - 5 for q3
 q2_minus = alpha(3);            %q- = alpha(M) (end of gait)
 q3_minus = gamma(3);
 
-a0 = -alpha(3); g0 = gamma(3)-1.0472;       
+a0 = -alpha(3); g0 = gamma(3)-gamma(1)/2;
 
 %qb_dot = db/ds*s_dot = M*(alpha(4) - alpha(3))*theta_dot-/(delta_theta)
 dq2_minus = M*(alpha(3) - alpha(2))*z_minus(2)/delq;
@@ -24,36 +24,36 @@ dq3_minus = M*(gamma(3) - gamma(2))*z_minus(2)/delq;
 a1 = dq2_minus*delq/(M*z_minus(2)) + a0;
 g1 = dq3_minus*delq/(M*z_minus(2)) + g0;
 
-alpha = [-f(5), -f(4), f(3:5)];      
+alpha = [-f(5), -f(4), f(3:5)];
 gamma = [f(8)-f(6)/2, 2*f(8)-f(6)/2-f(7), f(6:8)];
 
 a = [alpha, gamma];
 
 %I.C. [thetas; velocities]
-x0 = [z_minus(1), q2_minus, q3_minus, z_minus(2), dq2_minus, dq3_minus];
+x_minus = [z_minus(1), q2_minus, q3_minus, z_minus(2), dq2_minus, dq3_minus];
 
 %Apply impact map
-x_plus = impact_map(x0);
-x_plus = x_plus(1:6)'; 
+x_plus = impact_map(x_minus);
+x_plus = x_plus(1:6)';
 
-z_plus = [x_plus(1), x_plus(4)];
-delq = z_plus(1) - z_minus(1);
+%z_plus = [x_plus(1), x_plus(4)];
+delq = x_plus(1) - x_minus(1);
 
 
-tstart = 0; tfinal = 10;                    %max time per swing
+tstart = 0; tfinal = 50;                    %max time per swing
 
 refine = 4; options = odeset('Events',@events,'Refine',refine);    %'OutputFcn',@odeplot,'OutputSel',1,
 
 time_out = tstart; states = x_plus.'; foot = [];
 
 %Simlulation
-for i = 1:10                 %max number of steps allowed (depends on tfinal)
+for i = 1:30                 %max number of steps allowed (depends on tfinal)
     %Solve until the first terminal event.
     [t,x] = ode45(@(t,x) dx_vector_field(t,x,a), [tstart tfinal], x_plus, options);
     nt = length(t);
     
     %Setting the new initial conditions based on impact map
-    x_plus = impact_map(x(nt,:));
+    [x_plus,~] = impact_map(x(nt,:));
     
     x_plus = x_plus(1:6);       % Only positions and velocities needed as initial conditions
     
@@ -71,7 +71,7 @@ for i = 1:10                 %max number of steps allowed (depends on tfinal)
     
     %Save data
     time_out = [time_out; t(2:nt)];
-    states = [states;x(2:nt,:)];%size([dhdq; D(1,1:3)])
+    states = [states;x(2:nt,:)];
     foot = [foot; (x(1,1)+x(end,1))/2*ones(length(x(:,1)),1) + states(end,1)  - x(end,1)]; %remember foot location
     
 end
@@ -106,10 +106,10 @@ hold off
 %}
 
 %Event function
-    function [limits,isterminal,direction] = events(~,z)
+    function [limits,isterminal,direction] = events(~,x)
         
-        q1 = z(1);
-        s = (z_plus(1) - q1)/delq;   %normalized general coordinate
+        q1 = x(1);
+        s = (x_plus(1) - q1)/delq;   %normalized general coordinate
         
         limits(1) = s-1; 	%check when gait is at the end
         limits(2) = s+0.1;      %check if gait rolls back to 0
@@ -141,7 +141,7 @@ hold off
         
         %s variable used in bezier polynomial
         %delq = deg2rad(30);             %difference between min and max q1
-        s = (z_plus(1) - q1)/delq;         %normalized general coordinate
+        s = (x_plus(1) - q1)/delq;         %normalized general coordinate
         
         b2 = bezier(s,4,a2);
         b3 = bezier(s,4,a3);
@@ -151,14 +151,14 @@ hold off
         
         dh_dx = [ 0, 1, 0, 0, 0, 0;...
             0, 0, 1, 0, 0, 0];
-        dh_dx(1,1) = ((10000*a22*((2500*q1)/1309 - 3/2)^3)/1309 - (10000*a21*((2500*q1)/1309 - 3/2)^3)/1309 + (10000*a24*((2500*q1)/1309 - 1/2)^3)/1309 - (10000*a25*((2500*q1)/1309 - 1/2)^3)/1309 + (30000*a22*((2500*q1)/1309 - 1/2)*((2500*q1)/1309 - 3/2)^2)/1309 + (30000*a24*((2500*q1)/1309 - 1/2)^2*((2500*q1)/1309 - 3/2))/1309 - 6*a23*((2500*q1)/1309 - 3/2)^2*((12500000*q1)/1713481 - 2500/1309) - 6*a23*((2500*q1)/1309 - 1/2)^2*((12500000*q1)/1713481 - 7500/1309))/delq;
-        dh_dx(2,1) = ((10000*a32*((2500*q1)/1309 - 3/2)^3)/1309 - (10000*a31*((2500*q1)/1309 - 3/2)^3)/1309 + (10000*a34*((2500*q1)/1309 - 1/2)^3)/1309 - (10000*a35*((2500*q1)/1309 - 1/2)^3)/1309 + (30000*a32*((2500*q1)/1309 - 1/2)*((2500*q1)/1309 - 3/2)^2)/1309 + (30000*a34*((2500*q1)/1309 - 1/2)^2*((2500*q1)/1309 - 3/2))/1309 - 6*a33*((2500*q1)/1309 - 3/2)^2*((12500000*q1)/1713481 - 2500/1309) - 6*a33*((2500*q1)/1309 - 1/2)^2*((12500000*q1)/1713481 - 7500/1309))/delq;
+        dh_dx(1,1) = 4*a24*s^3 - 4*a25*s^3 - 4*a21*(s - 1)^3 + 4*a22*(s - 1)^3 + 12*a22*s*(s - 1)^2 - 12*a23*s*(s - 1)^2 + 12*a24*s^2*(s - 1) - 6*a23*s^2*(2*s - 2);
+        dh_dx(2,1) = 4*a34*s^3 - 4*a35*s^3 - 4*a31*(s - 1)^3 + 4*a32*(s - 1)^3 + 12*a32*s*(s - 1)^2 - 12*a33*s*(s - 1)^2 + 12*a34*s^2*(s - 1) - 6*a33*s^2*(2*s - 2);
         
         Lfh = dh_dx*Fx;
         
-        dLfh = [ -dq1*((75000000*a21*((2500*q1)/1309 - 3/2)^2)/1713481 + (75000000*a23*((2500*q1)/1309 - 1/2)^2)/1713481 - (150000000*a22*((2500*q1)/1309 - 3/2)^2)/1713481 - (150000000*a24*((2500*q1)/1309 - 1/2)^2)/1713481 + (75000000*a23*((2500*q1)/1309 - 3/2)^2)/1713481 + (75000000*a25*((2500*q1)/1309 - 1/2)^2)/1713481 - (30000*a24*((2500*q1)/1309 - 3/2)*((12500000*q1)/1713481 - 2500/1309))/1309 - (30000*a22*((2500*q1)/1309 - 1/2)*((12500000*q1)/1713481 - 7500/1309))/1309 + 12*a23*((12500000*q1)/1713481 - 2500/1309)*((12500000*q1)/1713481 - 7500/1309)), 0, 0, (10000*a22*((2500*q1)/1309 - 3/2)^3)/1309 - (10000*a21*((2500*q1)/1309 - 3/2)^3)/1309 + (10000*a24*((2500*q1)/1309 - 1/2)^3)/1309 - (10000*a25*((2500*q1)/1309 - 1/2)^3)/1309 + (30000*a22*((2500*q1)/1309 - 1/2)*((2500*q1)/1309 - 3/2)^2)/1309 + (30000*a24*((2500*q1)/1309 - 1/2)^2*((2500*q1)/1309 - 3/2))/1309 - 6*a23*((2500*q1)/1309 - 3/2)^2*((12500000*q1)/1713481 - 2500/1309) - 6*a23*((2500*q1)/1309 - 1/2)^2*((12500000*q1)/1713481 - 7500/1309), 1, 0;...
-            -dq1*((75000000*a31*((2500*q1)/1309 - 3/2)^2)/1713481 + (75000000*a33*((2500*q1)/1309 - 1/2)^2)/1713481 - (150000000*a32*((2500*q1)/1309 - 3/2)^2)/1713481 - (150000000*a34*((2500*q1)/1309 - 1/2)^2)/1713481 + (75000000*a33*((2500*q1)/1309 - 3/2)^2)/1713481 + (75000000*a35*((2500*q1)/1309 - 1/2)^2)/1713481 - (30000*a34*((2500*q1)/1309 - 3/2)*((12500000*q1)/1713481 - 2500/1309))/1309 - (30000*a32*((2500*q1)/1309 - 1/2)*((12500000*q1)/1713481 - 7500/1309))/1309 + 12*a33*((12500000*q1)/1713481 - 2500/1309)*((12500000*q1)/1713481 - 7500/1309)), 0, 0, (10000*a32*((2500*q1)/1309 - 3/2)^3)/1309 - (10000*a31*((2500*q1)/1309 - 3/2)^3)/1309 + (10000*a34*((2500*q1)/1309 - 1/2)^3)/1309 - (10000*a35*((2500*q1)/1309 - 1/2)^3)/1309 + (30000*a32*((2500*q1)/1309 - 1/2)*((2500*q1)/1309 - 3/2)^2)/1309 + (30000*a34*((2500*q1)/1309 - 1/2)^2*((2500*q1)/1309 - 3/2))/1309 - 6*a33*((2500*q1)/1309 - 3/2)^2*((12500000*q1)/1713481 - 2500/1309) - 6*a33*((2500*q1)/1309 - 1/2)^2*((12500000*q1)/1713481 - 7500/1309), 0, 1];
-        
+        dLfh = [-dq1*(12*a23*s^2 - 24*a24*s^2 + 12*a25*s^2 + 12*a21*(s - 1)^2 - 24*a22*(s - 1)^2 + 12*a23*(s - 1)^2 - 24*a24*s*(s - 1) - 12*a22*s*(2*s - 2) + 24*a23*s*(2*s - 2)), 0, 0, 4*a24*s^3 - 4*a25*s^3 - 4*a21*(s - 1)^3 + 4*a22*(s - 1)^3 + 12*a22*s*(s - 1)^2 - 12*a23*s*(s - 1)^2 + 12*a24*s^2*(s - 1) - 6*a23*s^2*(2*s - 2), 1, 0;...
+            -dq1*(12*a33*s^2 - 24*a34*s^2 + 12*a35*s^2 + 12*a31*(s - 1)^2 - 24*a32*(s - 1)^2 + 12*a33*(s - 1)^2 - 24*a34*s*(s - 1) - 12*a32*s*(2*s - 2) + 24*a33*s*(2*s - 2)), 0, 0, 4*a34*s^3 - 4*a35*s^3 - 4*a31*(s - 1)^3 + 4*a32*(s - 1)^3 + 12*a32*s*(s - 1)^2 - 12*a33*s*(s - 1)^2 + 12*a34*s^2*(s - 1) - 6*a33*s^2*(2*s - 2), 0, 1];
+ 
         epsilon = 0.1; alp = 0.9;
         
         %scaling
@@ -173,14 +173,12 @@ hold off
         v = 1/epsilon^2*psi;
         
         %{
-%PD control
-q2_d = deg2rad(30); q3_d = deg2rad(180); dq2_d = 2; dq3_d = 1;
-e = [q2 - q2_d; q3 - q3_d]; de = [dq2 - dq2_d; dq3 - dq3_d];
-Kp = [1000, 0; 0 1000];
-Kd = [1000, 0; 0 1000];
-v = -Kp*e - Kd*de;
-
-%v = -[2000; 2000];
+        %PD control
+        q2_d = 0; q3_d = deg2rad(150); dq2_d = -1; dq3_d = -1;
+        e = [q2 - q2_d; q3 - q3_d]; de = [dq2 - dq2_d; dq3 - dq3_d];
+        Kp = [1000, 0; 0 1000];
+        Kd = [1000, 0; 0 1000];
+        v = Kp*e - Kd*de;
         %}
         
         %u = LgLfh^-1*(v - L2fh) needed to cancel non-linearity
@@ -189,6 +187,28 @@ v = -Kp*e - Kd*de;
         dx = Fx + Gx*u;
         
     end
+    
+%{
+    function q = map_z_to_x(z,a)
+        
+        q1 = z(1);
+        dq1 = z(2);
+        a2 = a(1:5); a3 = a(6:end);
+        
+        M = 4;
+        
+        s = (x_plus(1) - q1)/delq;   %normalized general coordinate
+        
+        q2 = bezier(s,M,a2);
+        q3 = bezier(s,M,a3)';
+        
+        dq2 = d_ds_bezier(s,M,a2)*dq1/delq;
+        dq3 = d_ds_bezier(s,M,a3)*dq1/delq;
+        
+        q = [z(1), q2, q3, z(2), dq2, dq3];
+        
+    end
+%}
 
     function [hip_posx, leg1, leg2, torso] = motion(t,x)
         
